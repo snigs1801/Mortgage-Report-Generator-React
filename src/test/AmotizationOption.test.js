702fd052702123ccd1ac496ee0125727c7b5166b
snigs1801 <61434403+snigs1801@users.noreply.github.com>
2020-09-11 22:37:07 -0400

    Update AmotizationOption.test.js

--- src/test/AmotizationOption.test.js
+++ src/test/AmotizationOption.test.js
@@ -2,7 +2,6 @@ import React from 'react';import Enzyme, { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import AmotizationOption from "../components/AmotizationOption"; 
 
-
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('AmotizationOption component Test Cases', () => {
