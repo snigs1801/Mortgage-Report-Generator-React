1149c8e57ec2d7c0644b0c0893e63dfc3cacdc29
snigs1801 <61434403+snigs1801@users.noreply.github.com>
2020-09-11 22:39:01 -0400

    Update utils.test.js

--- src/test/utils.test.js
+++ src/test/utils.test.js
@@ -4,7 +4,6 @@ test('calculatePrinciple', () => {
     expect(calculateMonthlyPayment(100000, 5, 20, 20)).toBe(527.96);
 });
 
-
 test('calculatePrinciple', () => {
 	const data = generateSummaryData(100000,20,527.96)
     expect(data.length).toBe(5);
